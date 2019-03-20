6caf1a8d9c52aed088a704473ef118a55b637185
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2019-03-19 19:00:32 -0500

    array found dict

--- MarvelJson/ViewController.swift
+++ MarvelJson/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
+struct WebsiteDescription: Decodable {
+    let kind: String?
+    let data: [Text]?
+//    let data: [Details]?
+}
+
+struct Text: Decodable {
+    let mohash: String?
+    let dist: Int?
+    let children: [Children]?
+}
+
+struct Children: Decodable {
+    let kind: String?
+    let data: [Contex]?
+}
+struct Contex: Decodable {
+    let subredit: String?
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        let jasonUrlString = "https://www.reddit.com/r/marvelstudios.json"
+        
+        guard let url = URL(string: jasonUrlString) else
+        { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+
+            guard let data = data else { return }
+            
+            do {
+                let movies = try JSONDecoder().decode([WebsiteDescription].self, from: data)
+                print(movies[0])
+                
+            } catch let jsonErr {
+                print("error serializing", jsonErr)
+            }
+            
+        }.resume()
     }
-
-
 }
 
